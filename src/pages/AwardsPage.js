55a7acf27a32102a2cfd8770f1b857140cacb0e0
akotadi <manuel_7795@hotmail.com>
2019-08-09 15:05:06 -0500

    Adding awards.

--- src/pages/AwardsPage.js
+++ src/pages/AwardsPage.js
@@ -13,14 +13,16 @@ const images = {
         { id: 2, src: './assets/content/images/awards/award4th.jpg', title: 'Award 4th Semester' },
         { id: 3, src: './assets/content/images/awards/award3rd.jpg', title: 'Award 3rd Semester' },
         { id: 4, src: './assets/content/images/awards/award2nd.jpg', title: 'Award 2nd Semester' },
+        { id: 5, src: './assets/content/images/awards/award1st.jpg', title: 'Award 1st Semester' },
     ],
     idioms: [
-        { id: 5, src: './assets/content/images/awards/Certificato1.jpg', title: 'Certificato d\'italino' },
-        { id: 6, src: './assets/content/images/awards/TOEFL.jpg', title: 'Certificate TOEFL ITP' },
+        { id: 6, src: './assets/content/images/awards/Certificato1.jpg', title: 'Certificato d\'italino' },
+        { id: 7, src: './assets/content/images/awards/TOEFL.jpg', title: 'Certificate TOEFL ITP' },
     ],
     contest: [
-        { id: 7, src: './assets/content/images/awards/3rdPlace-Contest.jpg', title: 'Award 3rd Place - ACM Regional Contest 2nd date' },
-        { id: 8, src: './assets/content/images/awards/contestESCOM2018.jpg', title: 'Certificate of assistance to ESCOM Contest 2018' },
+        { id: 8, src: './assets/content/images/awards/3rdPlace-Contest.jpg', title: 'Award 3rd Place - ACM Regional Contest 2nd date' },
+        { id: 9, src: './assets/content/images/awards/contestESCOM2019.jpg', title: 'Certificate of assistance to ESCOM Contest 2019' },
+        { id: 10, src: './assets/content/images/awards/contestESCOM2018.jpg', title: 'Certificate of assistance to ESCOM Contest 2018' },
     ],
 }
 
