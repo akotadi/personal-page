6b14f265de4009c07f7fe3b1fe138e8dd6aa16cf
akotadi <manuel_7795@hotmail.com>
2022-01-04 20:54:24 -0600

    Initial refactor setup

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  arrowParens: 'avoid',
+  objectPropertyNewline: 'allowAllPropertiesOnSameLine',
+  endOfLine: 'auto',
+  bracketSameLine: false,
+};
